1d9f16ccc4d1aa02230c242645e49fd6a375de7f
Alexis Guzman <spartanthelexvro@gmail.com>
2025-02-02 21:09:29 -0600

    Creando repository books

--- src/app/core/repositories/book.repository.ts
+++ src/app/core/repositories/book.repository.ts
@@ -2,8 +2,11 @@ import { Observable } from 'rxjs';
 import { IBook } from '../domain/books.model';
 
 export abstract class BookRepository {
-    // Ingerfaz de los casos de uso haciendo referencia a go
-    abstract createBook(book: IBook): Observable<number>;
-    abstract getAllBooks(): Observable<IBook[]>;
-    
+  // Ingerfaz de los casos de uso haciendo referencia a go
+  abstract createBook(book: IBook): Observable<number>;
+  abstract getAllBooks(): Observable<IBook[]>;
+  abstract getBookById(id_book: number): Observable<IBook[]>;
+  abstract getBookByTitle(title: string): Observable<IBook[]>;
+  abstract UpdateBook(id_book: number, book: IBook): Observable<number>;
+  abstract DeleteBook(id_book: number): Observable<number>;
 }

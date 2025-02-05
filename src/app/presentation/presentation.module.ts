8909ce12ba7a24751b2dff10098ed5dec1291d67
Alexis Guzman <spartanthelexvro@gmail.com>
2025-02-04 23:57:36 -0600

    Pagina principal lista UI

--- src/app/presentation/presentation.module.ts
+++ src/app/presentation/presentation.module.ts
@@ -2,18 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BooksComponent } from './books/book-form/books.component';
 import { FormsModule } from '@angular/forms';
-import { ListComponent } from './books/list/list.component';
 import { PageBookComponent } from './books/page-book/page-book.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SearchComponent } from './books/search/search.component';
+import { CardBookComponent } from './books/card-book/card-book.component';
+import { BackToTopComponent } from './shared/back-to-top/back-to-top.component';
 
 @NgModule({
   declarations: [
     BooksComponent,
-    ListComponent,
     PageBookComponent,
     HeaderComponent,
     SearchComponent,
+    CardBookComponent,
+    BackToTopComponent,
   ],
   imports: [CommonModule, FormsModule],
   exports: [HeaderComponent],
